d7593a9349b2a70ab0a2325cfc7560d06761fbd8
manmohan.menon <manmohan.menon@india.nec.com>
2020-08-17 20:00:28 +0900

    adding initial commit along with code challenge_01

---
+++ Challenge.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Challenge {
+    public static void main(String[] args) {
+        int a, b, c;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            System.out.println("Enter first number:");
+            a = Integer.parseInt(br.readLine());
+            System.out.println("Enter second number:");
+            b = Integer.parseInt(br.readLine());
+            System.out.println("Enter third number:");
+            c = Integer.parseInt(br.readLine());
+            System.out.println("you entered the following numbers: " + "/n first number: " + a + "/n second number: "
+                    + b + "/n third number: " + c);
+        } catch (IOException e) {
+
+            System.out.println(e);
+        }
+    }
+
+}
