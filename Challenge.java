8df4346610ecc804ac127670f8e8eee38c0dfa1e
manmohan.menon <manmohan.menon@india.nec.com>
2020-08-25 12:01:11 +0900

    first excerise using scanner class , removed try catch clause IOException is never thrown in body of corresponding try statement

--- Challenge.java
+++ Challenge.java
@@ -1,22 +1,26 @@
 import java.io.*;
-
+import java.util.Scanner;
 public class Challenge {
     public static void main(String[] args) {
         int a, b, c;
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        try {
+        Scanner scan = new Scanner(System.in);
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       // try {
             System.out.println("Enter first number:");
-            a = Integer.parseInt(br.readLine());
+            //a = Integer.parseInt(br.readLine());
+            a = scan.nextInt();
             System.out.println("Enter second number:");
-            b = Integer.parseInt(br.readLine());
+            //b = Integer.parseInt(br.readLine());
+            b=scan.nextInt();
             System.out.println("Enter third number:");
-            c = Integer.parseInt(br.readLine());
+            //c = Integer.parseInt(br.readLine());
+            c=scan.nextInt();
             System.out.println("you entered the following numbers: " + "\n first number: " + a + "\n second number: "
                     + b + "\n third number: " + c);
-        } catch (IOException e) {
+       // } catch (IOException e) {
 
-            System.out.println(e);
-        }
+        //    System.out.println(e);
+       // }
     }
 
 }
